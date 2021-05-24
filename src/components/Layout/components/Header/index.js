e006ce131c14ab49575a481f6bba4b18a013428a
Zubair-Ashraf <zubair8767844@gmail.com>
2021-05-25 00:32:06 +0500

    Remove unsed imports

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 export const Header = () => {
   return (
