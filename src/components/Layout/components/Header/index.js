a4cc73ce905598d24eb7387cb4d97b1e1a6c9c16
Zubair-Ashraf <zubair8767844@gmail.com>
2021-05-25 10:53:21 +0500

    Add react-router-bootstrap dependency

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 
 export const Header = () => {
   return (
     <Navbar bg='dark' variant='dark' expand='lg' fixed='top'>
       <Container fluid>
-        <Navbar.Brand href='/'>React E-Commerce</Navbar.Brand>
+        <LinkContainer to='/'>
+          <Navbar.Brand>React E-Commerce</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='ms-auto'>
-            <Nav.Link href='#home'>
-              <i className='fas fa-shopping-cart fa-sm me-2' />
-              Cart
-            </Nav.Link>
-            <Nav.Link href='#link'>
-              <i className='fas fa-user fa-sm me-2' />
-              Sign In
-            </Nav.Link>
+            <LinkContainer to='/cart'>
+              <Nav.Link>
+                <i className='fas fa-shopping-cart fa-sm me-2' />
+                Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link>
+                <i className='fas fa-user fa-sm me-2' />
+                Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
