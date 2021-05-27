4d8ccea0d2da4d03fcf24a83a28c9fb3d52a8e08
Zubair-Ashraf <zubair8767844@gmail.com>
2021-05-27 10:19:22 +0500

    Fetch products from actual database

--- server/src/api/products/products.controller.js
+++ server/src/api/products/products.controller.js
@@ -1,13 +1,16 @@
+import { Product } from './products.model';
 import products from '../../constants/products';
 
 export const getProducts = async (_, res) => {
+  const products = await Product.find({});
+
   res.send(products);
 };
 
 export const getProduct = async (req, res) => {
   const productId = req.params.id;
 
-  const product = products.find(({ _id }) => _id === productId);
+  const product = await Product.findById(productId);
 
   res.send(product);
 };
--- server/src/connections/mongo.connection.js
+++ server/src/connections/mongo.connection.js
@@ -4,13 +4,16 @@ import chalk from 'chalk';
 const connectionString = process.env.MONGO_CONNECTION_STRING;
 
 export const mongoConnection = async () => {
-  await mongoose.connect(
-    connectionString,
-    { useUnifiedTopology: true, useNewUrlParser: true },
-    function (err) {
-      if (!err)
-        console.log(chalk.bgGreen.bold('Database connected successfully'));
-      else console.log(chalk.bgRed.bold('Database connection failed'));
-    }
-  );
+  console.log(chalk.bgYellow.bold('Database is connecting. Please wait...'));
+  try {
+    await mongoose.connect(connectionString, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(chalk.bgGreen.bold('Database connected successfully'));
+  } catch (error) {
+    console.log(chalk.bgRed.bold('Database connection failed'));
+    process.exit(1);
+  }
 };
